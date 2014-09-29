58eb3c04defb49e65c6d76a9d7beb6f501f78d1d
a <a>
2014-09-29 21:36:13 +0300

    Mayhem

--- _Upf/app_logic/_Views/backend/standard/layouts/docs/TestBackend.blade.php
+++ _Upf/app_logic/_Views/backend/standard/layouts/docs/TestBackend.blade.php
@@ -1,1 +1,77 @@
 @extends($template)
+
+@section('main')
+<section class="Content">
+<section class="Content-Inner">
+    <h3 class="Heading Secondary">Box</h3>
+    <p>Box &mdash; универсальный модуль.</p>
+    <p>При необходимости может быть снабжен интерактивными функциями:</p>
+    <ul>
+        <li>Сворачивание контента</li>
+        <li>Удаление себя со страницы</li>
+    </ul>
+    <section class="Node-Wrap">
+<pre><code class="html">&lt;article class=&quot;Box&quot;&gt;
+     &lt;header&gt;
+        &lt;h4 class=&quot;Box-Title&quot;&gt;Заголовок&lt;/h4&gt;
+        &lt;ul class=&quot;Box-Tools&quot;&gt;
+           &lt;li&gt;&lt;a href=&quot;&quot;&gt;&lt;span class=&quot;fa fa-chevron-down&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;
+           &lt;li&gt;&lt;a href=&quot;&quot;&gt;&lt;span class=&quot;fa fa-close&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;
+        &lt;/ul&gt;
+     &lt;/header&gt;
+     &lt;div class=&quot;Box-Content&quot;&gt;
+        Содержимое
+     &lt;/div&gt;
+&lt;/article&gt;</code>
+</pre>
+    </section>
+    <section class="Node-Wrap Row Split">
+        <article class="Box Grid-XS-6 Grid-MD-4 Grid-LR-3">
+            <header>
+                <h4 class="Box-Title">Box</h4>
+                <ul class="Box-Tools">
+                    <li><a href=""><span class="fa fa-chevron-down"></span></a></li>
+                    <li><a href=""><span class="fa fa-close"></span></a></li>
+                </ul>
+            </header>
+            <div class="Box-Content">
+                Содержимое
+            </div>
+        </article>
+        <article class="Box Outlined Grid-XS-6 Grid-MD-4 Grid-LR-3">
+            <header>
+                <h4 class="Box-Title">Box с границей</h4>
+            </header>
+            <div class="Box-Content">
+                <p>Класс: <strong>.Outlined</strong></p>
+                <p>Этот Box стилизован при помощи рамки.</p>
+                <p>Также у него отсутствуют элементы интерактива</p>
+            </div>
+        </article>
+        <article class="Box Solid Grid-XS-6 Grid-MD-4 Grid-LR-3">
+            <header>
+                <h4 class="Box-Title">Box с акцентом на заголовке</h4>
+                <ul class="Box-Tools">
+                    <li><a href=""><span class="fa fa-chevron-down"></span></a></li>
+                </ul>
+            </header>
+            <div class="Box-Content">
+                <p>Класс: <strong>Solid</strong></p>
+                <p>Этот Box имеет выделенный заголовок</p>
+                <p>Цвет фона заголовка берется из переменной $Clr-Main</p>
+            </div>
+        </article>
+        <article class="Box Gradient Info Grid-XS-6 Grid-MD-4 Grid-LR-3">
+            <header>
+                <h4 class="Box-Title">Box с градиентом и особым цветом</h4>
+            </header>
+            <div class="Box-Content">
+                <p>Класс: <strong>.Gradient .Info</strong></p>
+                <p>Цвет градиента берется из переменной $Clr-Info</p>
+            </div>
+        </article>
+    </section>
+</section>
+</section>
+
+@endsection
