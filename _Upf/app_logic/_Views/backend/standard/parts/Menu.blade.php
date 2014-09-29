1f4ff8e0c062236d1ac7bbacfb029810a482220b
Олег <office@unrealprojects.com>
2014-09-29 10:01:01 +0300

    Global Changes

--- _Upf/app_logic/_Views/backend/standard/parts/Menu.blade.php
+++ _Upf/app_logic/_Views/backend/standard/parts/Menu.blade.php
@@ -7,41 +7,42 @@
         <li class="Menu-Category-Item">
             <a class="Menu-Category-Link Active" href="#"><span class="fa fa-sitemap"></span>Разделы</a><span class="Menu-Category-Icon fa fa-angle-right"></span>
             <ul class="Menu-Subcategory">
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-share-alt"></span>Каталог стройтехники</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-share-alt"></span>Аренда стройтехники</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-share-alt"></span>Запчасти и сервис</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-share-alt"></span>Арендодатели</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-calendar"></span>Новости</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-file"></span>Статические страницы</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/section/catalog"><span class="fa fa-share-alt"></span>Каталог стройтехники</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/section/rent"><span class="fa fa-share-alt"></span>Аренда стройтехники</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/section/parts"><span class="fa fa-share-alt"></span>Запчасти и сервис</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/section/users"><span class="fa fa-share-alt"></span>Пользователи(Арендодатели)</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/section/articles"><span class="fa fa-calendar"></span>Статьи</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/section/pages"><span class="fa fa-file"></span>Статические страницы</a></li>
             </ul>
         </li>
 
         <li class="Menu-Category-Item">
             <a class="Menu-Category-Link" href="#"><span class="fa fa-pencil-square-o"></span>Фильтры</a><span class="Menu-Category-Icon fa fa-angle-right"></span>
             <ul class="Menu-Subcategory">
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link Active" href="#"><span class="fa fa-pencil"></span>Категории</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Регионы</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Теги</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Параметры</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link Active" href="/backend/filter/categories"><span class="fa fa-pencil"></span>Категории</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/filter/regions"><span class="fa fa-pencil"></span>Регионы</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/filter/tags"><span class="fa fa-pencil"></span>Теги</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/filter/params"><span class="fa fa-pencil"></span>Параметры</a></li>
             </ul>
         </li>
 
         <li class="Menu-Category-Item">
             <a class="Menu-Category-Link" href="#"><span class="fa fa-cog"></span>Система</a><span class="Menu-Category-Icon fa fa-angle-right"></span>
             <ul class="Menu-Subcategory">
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Администраторы</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Комментарии</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-puzzle-piece"></span>Карта сайта</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-puzzle-piece"></span>Файлы</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-file"></span>Настройки</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/system/administrators"><span class="fa fa-pencil"></span>Администраторы</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/system/comments"><span class="fa fa-pencil"></span>Комментарии</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/system/map"><span class="fa fa-puzzle-piece"></span>Карта сайта</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/system/files"><span class="fa fa-puzzle-piece"></span>Файлы</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="/backend/system/settings"><span class="fa fa-file"></span>Настройки</a></li>
             </ul>
         </li>
 
         <li class="Menu-Category-Item">
             <a class="Menu-Category-Link" href="#"><span class="fa fa-cog"></span>Документация</a><span class="Menu-Category-Icon fa fa-angle-right"></span>
             <ul class="Menu-Subcategory">
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Hardcore Cms</a></li>
-                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Unreal Projects Framework</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Upf CMS</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Upf js</a></li>
+                <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="#"><span class="fa fa-pencil"></span>Upf css</a></li>
                 <li class="Menu-Subcategory-Item"><a class="Menu-Subcategory-Link" href="http://laravel.com/docs"><span class="fa fa-pencil"></span>Laravel Framework</a></li>
 
             </ul>
