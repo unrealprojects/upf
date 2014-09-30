66a91c7f3aa9cd71c4d0d61114d200a59a71373b
a <a>
2014-09-30 14:12:41 +0300

    Mayhem

--- _Upf/app_logic/_Views/backend/standard/layouts/system/Auth.blade.php
+++ _Upf/app_logic/_Views/backend/standard/layouts/system/Auth.blade.php
@@ -1,21 +1,33 @@
 @extends('backend.standard.empty')
 @section('main')
-    <section class="Content">
-        <section class="Content-Inner">
-            <h3 class="Heading Secondary">Авторизация</h3>
-            <form id="Auth">
-                <div class="Control-Group">
-                    <label for="field_login">Логин</label>
-                    <input name="login" id="field_login" type="text" placeholder="Логин"/>
-                </div>
-                <div class="Control-Group">
-                    <label for="field_password">Пароль</label>
-                    <input name="password" id="field_password" type="password" placeholder="Пароль"/>
-                </div>
-                <input class="Button Huge Round" type="submit" value="Вход"/>
-            </form>
+    <div class="Auth-Wrap">
+        <section class="Auth">
+            <h3 class="Heading Secondary Icon">Авторизация</h3>
+                <form class="Vertical" id="Auth">
+                    <div class="Control-Group">
+                        <label for="field_login">Логин</label>
+                        <div class="Input-Group Icon Postfix">
+                            <input name="login" id="field_login" type="text"/>
+                            <span class="fa fa-user"></span>
+                        </div>
+                    </div>
+                    <div class="Control-Group">
+                        <div class="Label-Group">
+                            <label for="field_password">Пароль</label>
+                            <a class="SubLabel" href="#">Забыли пароль?</a>
+                        </div>
+                        <div class="Input-Group Icon Postfix">
+                            <input name="password" id="field_password" type="password"/>
+                            <span class="fa fa-lock"></span>
+                        </div>
+                    </div>
+                    <div class="Control-Group Checkbox Submit Offset">
+                        <input type="checkbox"/><label>Запомнить меня</label>
+                        <input class="Button Round" type="submit" value="Войти"/>
+                    </div>
+                </form>
         </section>
-    </section>
+    </div>
 @endsection
 
 @section('scripts')
