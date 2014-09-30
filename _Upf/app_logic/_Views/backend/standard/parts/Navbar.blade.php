4dc40596605f895228f54d16246465301dfa68dc
a <a>
2014-09-30 14:43:29 +0300

    Mayhem

--- _Upf/app_logic/_Views/backend/standard/parts/Navbar.blade.php
+++ _Upf/app_logic/_Views/backend/standard/parts/Navbar.blade.php
@@ -10,7 +10,13 @@
         <li><a href="#"><span class="fa fa-pencil fa-lg"></span></a></li>
         <li><a href="#"><span class="fa fa-trash fa-lg"></span></a></li>
         <li><a href="#"><span class="fa fa-briefcase fa-lg"></span></a></li>
-        <li class="Icon-Link Right"><a href="#">alexmayhem<span class="fa fa-angle-down"></span></a></li>
+        <li class="User-Menu Icon-Link Right">
+            <a class="Toggle" href="#">alexmayhem<span class="fa fa-angle-down"></span></a>
+            <ul class="Dropdown">
+                <li class="Icon"><a href="#"><span class="fa fa-user"></span>Кабинет</a></li>
+                <li class="Icon"><a href="#"><span class="fa fa-close"></span>Выйти</a></li>
+            </ul>
+        </li>
     </ul>
 </nav>
 <nav class="Breadcrumbs">
