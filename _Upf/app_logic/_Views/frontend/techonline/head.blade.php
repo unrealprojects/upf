1c1675ad432022bf4b03e0a4f2e8fa9c4ef880e7
a <a>
2014-10-11 00:39:57 +0300

    Mayhem

--- _Upf/app_logic/_Views/frontend/techonline/head.blade.php
+++ _Upf/app_logic/_Views/frontend/techonline/head.blade.php
@@ -1,4 +1,3 @@
 <link rel="icon" type="image/png" href="/img/techonline/favicon.ico" />
 <link rel="stylesheet" href="/scss/general/frontend/techonline/main.css"/>
 <link href='http://fonts.googleapis.com/css?family=Exo+2:400,400italic,500,500italic,700,700italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
