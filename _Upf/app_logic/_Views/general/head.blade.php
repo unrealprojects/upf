16fe465b597ec2542485774ff8959cd307a0d8cc
a <a>
2014-10-10 12:00:10 +0300

    Mayhem

--- _Upf/app_logic/_Views/general/head.blade.php
+++ _Upf/app_logic/_Views/general/head.blade.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link rel="stylesheet" href="/css/general/libs/jquery-ui.min.css"/>
-<link rel="stylesheet" type="text/css" href="/css/general/libs/fancybox/fancybox.min.css" />
+<link rel="stylesheet" type="text/css" href="/css/general/libs/fancybox/fancybox.css" />
 
 <!--[if lt IE 9]>
 <script src="/js/general/libs/ie8/html5shiv.js" type="text/javascript"></script>
